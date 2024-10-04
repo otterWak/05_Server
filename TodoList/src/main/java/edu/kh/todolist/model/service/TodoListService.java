78351a558ce40ec054d59ebe82c16fb35accb862
OtterWak <erichonggu@gmail.com>
2024-10-04 17:28:37 +0900

    20241004 JSP3

---
+++ TodoList/src/main/java/edu/kh/todolist/model/service/TodoListService.java
@@ -0,0 +1,23 @@
+package edu.kh.todolist.model.service;
+
+import java.util.List;
+
+import edu.kh.todolist.model.dto.Todo;
+
+public interface TodoListService {
+	// public abstract를 명시하지 않아도 public abstract으로 해석됨
+	
+	/** 할 일 목록 반환 서비스
+	 * @return tosoList
+	 */
+	/*public abstract*/List<Todo> todoListFullView(); // 추상메서드(interface)
+
+	/** 전달 받은 title, detail 이용하여 할 일 추가 서비스
+	 * @param title
+	 * @param detail
+	 * @return 추가된 index번호 or -1 반환
+	 */
+	int todoAdd(String title, String detail) throws Exception;
+
+	
+}
